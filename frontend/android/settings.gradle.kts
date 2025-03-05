0ec845bbf6b03c3ef7f67817ede6abe38b2e0d91
zakariem <zakariyem367@gmail.com>
2025-03-05 10:32:40 +0300

    Update Gradle wrapper and Flutter plugin version

--- frontend/android/settings.gradle.kts
+++ frontend/android/settings.gradle.kts
@@ -17,7 +17,7 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("dev.flutter.flutter-plugin-loader") version "1.1.0"
     id("com.android.application") version "8.7.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.22" apply false
 }
