e4a26cd2f76f80452ca6992cdbdba702f9354905
zakariem <zakariyem367@gmail.com>
2025-03-06 10:13:58 +0300

    Add authentication and role-based middleware for user access control

---
+++ backend/middlewares/auth-middleware.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+
+export default function authMiddleware(req, res, next) {
+  let token;
+
+  let authHeader = req.headers.authorization || req.headers.Authorization;
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    token = authHeader.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+      next();
+    } catch (err) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+  } else {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+}
